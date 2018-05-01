8a71cfc3298a8292f6866fcb078a9a2bb38308f7
Unknown <julius08aw@gmail.com>
2018-05-01 22:00:36 +0900

    add auto_control with move_speed and move_curve

--- scripts/auto_control.py
+++ scripts/auto_control.py
@@ -4,14 +4,15 @@ import rospy
 from std_msgs.msg import Float32
 
 rospy.init_node("auto_control")
-pub = rospy.Publisher('move_speed', Float32, queue_size=1000)
-pub = rospy.Publisher('move_time', Float32, queue_size=1000)
+pub_speed = rospy.Publisher('move_speed', Float32, queue_size=1000)
+pub_curve = rospy.Publisher('move_curve', Float32, queue_size=1000)
 
 rate = rospy.Rate(10)
 move_speed = 0.05
-move_curve = 0.3
+move_curve = 1 / 0.3
 
 while not rospy.is_shutdown():
-    pub.publish(move_speed)
-    pub.publish(move_curve)
+    print("%f", move_speed)
+    pub_speed.publish(move_speed)
+    pub_curve.publish(move_curve)
     rate.sleep()
