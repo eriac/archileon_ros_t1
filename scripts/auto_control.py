62619da990754dd83f42d6637f3783c6b08304fe
Unknown <julius08aw@gmail.com>
2018-05-04 01:52:54 +0900

    beautify

--- scripts/auto_control.py
+++ scripts/auto_control.py
@@ -8,8 +8,7 @@ from sympy import *
 import math
 import time
 
-x = Symbol("x")
-y = Symbol("y")
+
 world_target_position = [(0.5, 0.5), (0, 1.0), (-0.5, 1.5), (0, 2.0), (0.5, 2.5)]
 move_speed = 0.05
 
@@ -26,9 +25,8 @@ def callback(msg):
     # if world_target_position is []:
     #     print("END OF POSITION")
 
-    # if (timer.set is None and timer.start is None):
-    # if set_time <= time.time() - start_time:
-    print((timer.set -((time.time() - timer.start))))
+
+    # print((timer.set -((time.time() - timer.start))))
     if (timer.set <= (time.time() - timer.start)):
 
         print("//////////////////////////////////////////////////////")
@@ -61,9 +59,10 @@ def callback(msg):
         pr = np.array([pr_x, pr_y])
 
         if world_rob_theta < 0:
-            # world_rob_theta = - world_rob_theta
+            world_rob_theta = - world_rob_theta
             print("world_rob_theta is MINUS")
             print(world_rob_theta)
+            print(" ")
 
             cos = np.cos(world_rob_theta)
             sin = np.sin(world_rob_theta)
@@ -73,6 +72,9 @@ def callback(msg):
         if world_rob_theta >= 0:
 
             print("world_rob_theta is PLUS")
+            print(world_rob_theta)
+            print(" ")
+
             cos = np.cos(world_rob_theta)
             sin = np.sin(world_rob_theta)
             reverse_rotate = np.array([[cos, sin], [-sin, cos]])
@@ -103,10 +105,6 @@ def callback(msg):
         print("a is " + str(a))
         move_curve = a
 
-
-        print("a is " + str(a))
-        print(" ")
-
         # if  a + 0. 5 == rob_target_y:
         #     rad = math.pi / 2
         # elif rob_target_y < a:
@@ -116,13 +114,7 @@ def callback(msg):
         #     rad = math.atan2((rob_target_y - a), rob_target_x)
 
         rad = math.pi / 2
-
-
-        if move_curve > 0:
-            print("角度 " +str(math.degrees(rad)))
-            arc_circle = 2 * (move_curve) * math.pi * (math.degrees(rad)/360)
-        if move_curve < 0:
-            arc_circle = 2 * (-move_curve) * math.pi * (math.degrees(rad)/360)
+        arc_circle = abs(2 * (move_curve) * math.pi * (math.degrees(rad)/360))
 
         print("円弧の長さ " + str(arc_circle))
         move_time = arc_circle / move_speed
@@ -132,7 +124,6 @@ def callback(msg):
 
         print("動いてほしい時間 " + str(move_time))
         print("曲率" + str(1.0 / move_curve))
-
 
 
         count = 0
@@ -151,82 +142,6 @@ def callback(msg):
         # rospy.spin()
         print("START TIME " + str(timer.start))
         print("//////////////////////////////////////////////////////")
-
-    # else:
-        # print('NORMARL CALL')
-        # if (timer.set <= (time.time() - timer.start)):
-    # if (set_time <= (time.time() - start_time)):
-            # print("//////////////////////////////////////////////////////")
-            # print("Normal callback")
-            # print(timer.set -((time.time() + timer.start)))
-            # print(" ")
-            # world_rob_x = msg.data[0]
-            # world_rob_y = msg.data[1]
-            # world_rob_theta = msg.data[2]
-            #
-            # print("world_rob_x " + str(world_rob_x))
-            # print("world_rob_y " + str(world_rob_y))
-            # print(" ")
-            #
-            # print("world_target_position x " + str(world_target_position[0][0]))
-            # print("world_target_position y " + str(world_target_position[0][1]))
-            # print(" ")
-            # pr_x = world_target_position[0][0] - world_rob_x
-            # pr_y = world_target_position[0][1] - world_rob_y
-            #
-            # world_target_position.pop(0)
-            #
-            # pr = np.array([pr_x, pr_y])
-            #
-            # cos = np.cos(world_rob_theta)
-            # sin = np.sin(world_rob_theta)
-            #
-            # reverse_rotate = np.array([[cos, sin], [-sin, cos]])
-            # rob_target_position = np.dot(reverse_rotate, pr)
-            # rob_target_x = rob_target_position[0]
-            # rob_target_y = rob_target_position[1]
-            #
-            # print("rob_target_x  is " + str(rob_target_x))
-            # print("rob_target_y  is " + str(rob_target_y))
-            # print(" ")
-            #
-            # # x**2 + (y - a)**2 = a**2
-            # a = (rob_target_x**2 + rob_target_y**2)/ (2 * rob_target_y)
-            #
-            #
-            # move_curve = a
-            # # move_curve = 1
-            # print("a is " + str(a))
-            # print(" ")
-            # if rob_target_y == a:
-            #     rad = math.pi / 2
-            # elif rob_target_y < a:
-            #     rad = math.atan2((a - rob_target_y), rob_target_x)
-            #     # rad = math.atan2((a-y)/x)
-            # elif a < rob_target_y:
-            #     rad = math.atan2((rob_target_y - a), rob_target_x)
-            #
-            # print("角度 " +str(math.degrees(rad)))
-            # arc_circle = 2 * a * math.pi * (math.degrees(rad)/360)
-            #
-            # print("円弧の長さ " + str(arc_circle))
-            # move_time = arc_circle / move_speed
-            # timer.set_time(move_time)
-            # print("動いてほしい時間 " + str(move_time))
-            #
-            # print("曲率" + str(1.0 / move_curve))
-            # timer.start_time(time.time())
-            #
-            # rate = rospy.Rate(4)
-            # while not rospy.is_shutdown():
-            #     pub_curve.publish(1.0 / move_curve)
-            #     rate.sleep()
-            #
-            # print("START TIME " + str(timer.start))
-            # print("//////////////////////////////////////////////////////")
-            # print("x value is " + str(x))
-            # print("y value is " + str(y))
-            # print("theta value is " + str(world_theta))
 
 
 
