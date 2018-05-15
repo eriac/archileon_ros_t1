b855c6a87609cb871d24ec61cd6c86b2c5d0af45
Unknown <julius08aw@gmail.com>
2018-05-16 08:51:19 +0900

    modify tire position

--- src/move_driver.cpp
+++ src/move_driver.cpp
@@ -131,31 +131,31 @@ int main(int argc, char **argv){
                 std_msgs::Float32 sv;
                 std_msgs::Float32 mv;
                 if (i == 0 ){
-                  sv.data=-atan2(ws_pos[i][0],center_y-ws_pos[i][1]) + 0.0877*2;
+                  sv.data=-atan2(ws_pos[i][0],ws_pos[i][1]- center_y) + 0.0877*2;
                   float side_length = sqrt((ws_pos[i][1]-center_y)*(ws_pos[i][1]-center_y) + ws_pos[i][0] * ws_pos[i][0]);
-                  mv.data=-f_val1*(side_length - real_length)/real_length;
+                  mv.data=f_val1*(side_length - real_length)/real_length;
                 }
                 else if (i == 1 ){
-                  sv.data=-atan2(ws_pos[i][0],center_y-ws_pos[i][1]) - 0.0877*2;
+                  sv.data=-atan2(ws_pos[i][0],ws_pos[i][1]-center_y) - 0.0877*2;
                   float side_length = sqrt((ws_pos[i][1]-center_y)*(ws_pos[i][1]-center_y) + ws_pos[i][0] * ws_pos[i][0]);
-                  mv.data=-f_val1*(real_length - side_length)/real_length;
+                  mv.data=f_val1*(real_length - side_length)/real_length;
                 }
                 else if (i == 2 ){
-                  sv.data=-atan2(ws_pos[i][0],center_y-ws_pos[i][1]) - 0.0877;
+                  sv.data=-atan2(ws_pos[i][0],ws_pos[i][1]-center_y) - 0.0877;
                   float side_length = sqrt((ws_pos[i][1]-center_y)*(ws_pos[i][1]-center_y) + ws_pos[i][0] * ws_pos[i][0]);
-                  mv.data=-f_val1*(side_length - real_length)/real_length;
+                  mv.data=f_val1*(side_length - real_length)/real_length;
                 }
                 else if (i == 3 ){
-                  sv.data=-atan2(ws_pos[i][0],center_y-ws_pos[i][1]) - 0.0877 * 1/4;
+                  sv.data=-atan2(ws_pos[i][0],ws_pos[i][1]-center_y) - 0.0877 * 1/4;
                   float side_length = sqrt((ws_pos[i][1]-center_y)*(ws_pos[i][1]-center_y) + ws_pos[i][0] * ws_pos[i][0]);
-                  mv.data=-f_val1*(real_length - side_length)/real_length;
+                  mv.data=f_val1*(real_length - side_length)/real_length;
                 }
                 servo_pub[i].publish(sv);
                 motor_pub[i].publish(mv);
               }
               else if(4 <= i){
                 std_msgs::Float32 sv;
-                sv.data=atan2(ws_pos[i- 2][0], ws_pos[i- 2][1]-center_y) + (atan2(ws_pos[i- 2][0], ws_pos[i- 2][1]-center_y)) / 2;
+                sv.data=-(atan2(ws_pos[i- 2][0], ws_pos[i- 2][1]-center_y) + (atan2(ws_pos[i- 2][0], ws_pos[i- 2][1]-center_y)) / 2);
                 servo_pub[i].publish(sv);
               }
             }
