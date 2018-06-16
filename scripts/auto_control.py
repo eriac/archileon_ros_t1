054447c32a3419bca740b0da70acfc6b3e68c77e
Unknown <julius08aw@gmail.com>
2018-06-16 23:32:17 +0900

    get rid of a couple of bugs

--- scripts/auto_control.py
+++ scripts/auto_control.py
@@ -47,9 +47,72 @@ def position_callback(msg):
     world_rob_y = msg.data[1]
     world_rob_theta = msg.data[2]
 
-    print("world_rob_x " + str(world_rob_x))
-    print("world_rob_y " + str(world_rob_y))
-    print("world_rob_theta " + str(world_rob_theta))
+
+    # ロボの位置から相対的にノズルの位置を取ってくる
+    rob_bl_tube_rot_axis_position, rob_bl_tube_tip_position,rob_br_tube_rot_axis_position,rob_br_tube_tip_position = getTubePosition.cal(world_rob_x, world_rob_y, world_rob_theta)
+
+    # ノズルの回転の軸のworld座標系
+    bl_tube_rot_axis_position_x = rob_bl_tube_rot_axis_position[0]
+    bl_tube_rot_axis_position_y = rob_bl_tube_rot_axis_position[1]
+    # ノズル本体のworld座標系
+    bl_tube_tip_position_x = rob_bl_tube_tip_position[0]
+    bl_tube_tip_position_y = rob_bl_tube_tip_position[1]
+
+    #今のノズル本体の位置から、経路に対して最も近い点のインデックス２つを取ってくる
+    bl_tube_base_point_num, bl_tube_adj_point_num = getNearestPoint.search_value(bl_tube_tip_way_points, bl_tube_tip_position_x, bl_tube_tip_position_y)
+
+    #今のノズル本体の位置から、経路に対して最も近い２の点を取ってくる
+    bl_tube_tip_base_point_x = bl_tube_tip_way_points[bl_tube_base_point_num][0]
+    bl_tube_tip_base_point_y = bl_tube_tip_way_points[bl_tube_base_point_num][1]
+    bl_tube_adj_point_x = bl_tube_tip_way_points[bl_tube_adj_point_num][0]
+    bl_tube_adj_point_y = bl_tube_tip_way_points[bl_tube_adj_point_num][1]
+
+    ans_1, ans_2=getTubeTipInterSection.cal(bl_tube_rot_axis_position_x, bl_tube_rot_axis_position_y, tube_radius, bl_tube_tip_base_point_x, bl_tube_tip_base_point_y, bl_tube_adj_point_x, bl_tube_adj_point_y)
+    world_inter_section_x=ans_1[0][0]
+    world_inter_section_y=ans_1[0][1]
+
+    tube_axis_angle_radian=getTubeAngle.cal(bl_tube_rot_axis_position_x, bl_tube_rot_axis_position_y, bl_tube_tip_position_x, bl_tube_tip_position_y, world_inter_section_x, world_inter_section_y)
+
+    print("tube_radian")
+    print(math.degrees(tube_axis_angle_radian))
+
+    # tube_axis_angle_radian_array.data.append(tube_axis_angle_radian)
+
+    # # ノズルの回転の軸のworld座標系
+    # br_tube_rot_axis_position_x = rob_br_tube_rot_axis_position[0]
+    # br_tube_rot_axis_position_y = rob_br_tube_rot_axis_position[1]
+    # # ノズル本体のworld座標系
+    # br_tube_tip_position_x = rob_br_tube_tip_position[0]
+    # br_tube_tip_position_y = rob_br_tube_tip_position[1]
+    #
+    # #今のノズル本体の位置から、経路に対して最も近い点のインデックス２つを取ってくる
+    # br_tube_base_point_num, br_tube_adj_point_num = getNearestPoint.search_value(br_tube_tip_way_points, br_tube_tip_position_x, br_tube_tip_position_y)
+    #
+    # #今のノズル本体の位置から、経路に対して最も近い２の点を取ってくる
+    # br_tube_tip_base_point_x = br_tube_tip_way_points[br_tube_base_point_num][0]
+    # br_tube_tip_base_point_y = br_tube_tip_way_points[br_tube_base_point_num][1]
+    # br_tube_adj_point_x = br_tube_tip_way_points[br_tube_adj_point_num][0]
+    # br_tube_adj_point_y = br_tube_tip_way_points[br_tube_adj_point_num][1]
+    #
+    # ans_1, ans_2=getTubeTipInterSection.cal(br_tube_rot_axis_position_x, br_tube_rot_axis_position_y, tube_radius, br_tube_tip_base_point_x, br_tube_tip_base_point_y, br_tube_adj_point_x, br_tube_adj_point_y)
+    # world_inter_section_x=ans_1[0][0]
+    # world_inter_section_y=ans_1[0][1]
+    #
+    # tube_axis_angle_radian=getTubeAngle.cal(br_tube_rot_axis_position_x, br_tube_rot_axis_position_y, br_tube_tip_position_x, br_tube_tip_position_y, world_inter_section_x, world_inter_section_y)
+    #
+    # tube_axis_angle_radian_array.data.append(tube_axis_angle_radian)
+    #
+    # for num in range(3):
+    #     pub_tube_axis_angle.publish(tube_axis_angle_radian_array)
+    #
+
+    #今のロボの位置から、経路に対して最も近い点を２つ取ってくる
+    rob_base_point_num, rob_adj_point_num = getNearestPoint.search_value(rob_way_points, world_rob_x, world_rob_y)
+
+    rob_base_point_x = rob_way_points[rob_base_point_num][0]
+    rob_base_point_y = rob_way_points[rob_base_point_num][1]
+    rob_adj_point_x = rob_way_points[rob_adj_point_num][0]
+    rob_adj_point_y = rob_way_points[rob_adj_point_num][1]
 
 
     # do whileがないのでこう書く
@@ -63,7 +126,8 @@ def position_callback(msg):
 
 
     else:
-        if nowArea.judge(world_rob_x, world_rob_y, now_target_num=area_map.now_target_num) is True:
+        # pass
+        if nowArea.judge(world_rob_x, world_rob_y, now_target_num=area_map.now_target_num, way_points=rob_way_points) is True:
             area_map.now_target_num +=1
             print("ELSE")
             print("area_map.now_target_num " +str(area_map.now_target_num))
@@ -74,26 +138,26 @@ def position_callback(msg):
 
 
 
-
-
         # ロボットが経路からどれだけ離れたかを計算
-        # L_rob_points = abs(getDist_rob_points.cal(point_1_x=rob_base_point_x, point_1_y=rob_base_point_y, point_2_x=rob_adj_point_x, point_2_y=rob_adj_point_y, world_rob_x=world_rob_x, world_rob_y=world_rob_y))
+        L_rob_points = abs(getDist_rob_points.cal(point_1_x=rob_base_point_x, point_1_y=rob_base_point_y, point_2_x=rob_adj_point_x, point_2_y=rob_adj_point_y, world_rob_x=world_rob_x, world_rob_y=world_rob_y))
 
-        # if L_rob_points > 0.010:
-        #     move_curve = getMoveCurve.cal(world_rob_x, world_rob_y, world_rob_theta, world_target_x=area_map.target_point[0], world_target_y=area_map.target_point[1])
-        #     if move_curve > min_curve:
-        #         for i in range(3):
-        #             pub_curve.publish(1.0 / move_curve)
-        #             pub_speed.publish(move_speed)
-        #
-        #     else:
-        #         area_map.now_target_num +=1
-        #         move_curve = getMoveCurve.cal(world_rob_x, world_rob_y, world_rob_theta, world_target_x=area_map.target_point[0], world_target_y=area_map.target_point[1])
-        #         for i in range(3):
-        #             pub_curve.publish(1.0 / move_curve)
-        #             pub_speed.publish(move_speed)
-        #
-        #
+        print(L_rob_points)
+        if L_rob_points > 0.010:
+            move_curve = getMoveCurve.cal(world_rob_x, world_rob_y, world_rob_theta, world_target_x=area_map.main_points[area_map.now_target_num][0], world_target_y=area_map.main_points[area_map.now_target_num][1])
+            if move_curve > min_curve:
+                for i in range(3):
+                    pub_curve.publish(1.0 / move_curve)
+                    pub_speed.publish(move_speed)
+
+            else:
+                area_map.now_target_num +=1
+                move_curve = getMoveCurve.cal(world_rob_x, world_rob_y, world_rob_theta, world_target_x=area_map.main_points[area_map.now_target_num][0], world_target_y=area_map.main_points[area_map.now_target_num][1])
+
+                for i in range(3):
+                    pub_curve.publish(1.0 / move_curve)
+                    pub_speed.publish(move_speed)
+
+
 
 
 
--- scripts/nowArea.py
+++ scripts/nowArea.py
@@ -1,33 +1,27 @@
-def judge(world_rob_x, world_rob_y, now_target_num):
+def judge(world_rob_x, world_rob_y, now_target_num, way_points):
     import getWayPoints
     import getNearestPoint
     result = False
-    way_points = getWayPoints.read_rob()
+    way_points = way_points
 
     # print("now_target_num " +str(now_target_num))
 
     if now_target_num == 0:
-        print("Zero")
-        if world_rob_x > 1.001:
+        if world_rob_x > 1.0001:
             result = True
-    # elif now_target_num == 1:
-    #     print("One")
-    #     if world_rob_y > 0.501:
-    #         result = True
-    # elif now_target_num == 2:
-    #     print("Two")
-    #     if world_rob_x > 1.001:
-    #         result = True
-    # elif now_target_num == 3:
-    #     print("Three")
-    #     if world_rob_x < 0.001:
-    #         result = True
-    # elif now_target_num == 4:
-    #     print("Four")
-    #     if world_rob_y < 0.501:
-    #         result = True
-    # elif now_target_num == 5:
-    #     print("Five")
-    #     if world_rob_x > 0.501:
-    #         result = True
+    elif now_target_num == 1:
+        if world_rob_y > 0.5001:
+            result = True
+    elif now_target_num == 2:
+        if world_rob_x > 1.0001:
+            result = True
+    elif now_target_num == 3:
+        if world_rob_x < 0.0001:
+            result = True
+    elif now_target_num == 4:
+        if world_rob_y < 0.5001:
+            result = True
+    elif now_target_num == 5:
+        if world_rob_x > 0.5001:
+            result = True
     return result
