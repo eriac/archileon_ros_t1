bd6af4dc4f738379c1ab11549f847cae971abd1c
Unknown <julius08aw@gmail.com>
2018-07-03 18:40:42 +0900

    beautify

--- scripts/map_target_visualizer.py
+++ scripts/map_target_visualizer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import rospy
 from visualization_msgs.msg import Marker
 from std_msgs.msg import Float32MultiArray
@@ -7,46 +8,32 @@ from geometry_msgs.msg import Point
 
 
 
+
 def position_callback(float_array):
-    p = Point()
-    p.x = float_array[0]
-    p.y = float_array[0]
-    marker_data.points.apeend(p)
 
-
-
-rospy.init_node("map_target_visualizer")
-pub_map_target = rospy.Publisher("visualization_marker", Marker, queue_size = 10)
-sub_map_target = rospy.Subscriber("now_target_array", Float32MultiArray, position_callback)
-
-
-
-
-while not rospy.is_shutdown():
-    
     marker_data = Marker()
     marker_data.header.frame_id = "world"
     marker_data.header.stamp = rospy.Time.now()
     marker_data.id = 0
-
     marker_data.ns = "map_target_visualizer"
-
     marker_data.action = Marker.ADD
     marker_data.pose.orientation.w=1.0
-
+    marker_data.type = Marker.POINTS
     marker_data.color.r = 1.0
-    marker_data.color.g = 0.0
-    marker_data.color.b = 0.0
+    marker_data.color.b = 1.0
     marker_data.color.a = 1.0
-
     marker_data.scale.x = 0.1
     marker_data.scale.y = 0.1
-    
-    marker_data.lifetime = rospy.Duration()
+    p = Point()
+    p.x = float_array.data[0]
+    p.y = float_array.data[1]
+    marker_data.points.append(p)
+    for num in range(10):
+        pub_map_target.publish(marker_data)
 
-    marker_data.type = Marker.POINTS
 
-    rate = rospy.Rate(25)
 
-    pub_map_target.publish(marker_data)
-    rate.sleep()
+rospy.init_node("map_target_visualizer")
+pub_map_target = rospy.Publisher("map_target", Marker, queue_size = 10)
+sub_map_target = rospy.Subscriber("now_target_array", Float32MultiArray, position_callback)
+rospy.spin()
