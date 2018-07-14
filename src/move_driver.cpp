e1ca3430589f8b06f5fe863485ba8355aaa4f61d
Unknown <julius08aw@gmail.com>
2018-07-14 19:28:18 +0900

    fix speed bug

--- src/move_driver.cpp
+++ src/move_driver.cpp
@@ -91,6 +91,11 @@ int main(int argc, char **argv){
         float f_val0=curve_value/curve_factor;
         float f_val1=speed_value;
 
+        float center_y=1/f_val0;
+        float hypotense_sqrt = center_y * center_y + ws_pos[2][0] * ws_pos[2][0];
+        float hypotense = sqrt(hypotense_sqrt);
+
+
         if(-0.05<f_val0 && f_val0<0.05){//straight
             std_msgs::Float32 ss;
             ss.data=0;
@@ -103,75 +108,32 @@ int main(int argc, char **argv){
 
         }
         else if(f_val0>0){//curve
-
-            float center_y=1/f_val0;
-            float back_center_x = ws_pos[2][0];
-            float hypotense = center_y * center_y + back_center_x * back_center_x;
-            float real_length = sqrt(hypotense);
             for(int i=0;i<4;i++){
               std_msgs::Float32 sv;
               std_msgs::Float32 mv;
               if (i == 0 or i == 2){
                 sv.data=atan2(ws_pos[i][0],center_y-ws_pos[i][1]);
-                // mv.data=f_val1*(center_y-ws_pos[i][1])/center_y;
-
-                float side_length = sqrt((center_y - ws_pos[i][1])*(center_y - ws_pos[i][1]) + 
-                ws_pos[i][0] * ws_pos[i][0]);
-                mv.data=f_val1*(real_length-side_length)/real_length*1/2;
-
-                // float side_length = sqrt((center_y - ws_pos[i][1])*(center_y - ws_pos[i][1]) 
-                // - ws_pos[i][0] * ws_pos[i][0]);
-                // mv.data=f_val1*(side_length/center_y);
+                mv.data=f_val1*(hypotense-ws_pos[i][1])/hypotense*1/2;
               }
               else if(i == 1 or i == 3){
                 sv.data=atan2(ws_pos[i][0],center_y-ws_pos[i][1]);
-
-                // mv.data=-f_val1*(ws_pos[i][1]-center_y)/center_y;
-
-                float side_length = sqrt((center_y - ws_pos[i][1])*(center_y - ws_pos[i][1]) + 
-                ws_pos[i][0] * ws_pos[i][0]);
-                mv.data=-f_val1*(side_length - real_length)/real_length*1/2;
-
-                // float side_length = sqrt((center_y + ws_pos[i][1])*(center_y + ws_pos[i][1]) 
-                // - ws_pos[i][0] * ws_pos[i][0]);
-                // mv.data=f_val1*(side_length/center_y);side_length
+                mv.data=-f_val1*(ws_pos[i][1]-hypotense)/hypotense*1/2;
               }
               servo_pub[i].publish(sv);
               motor_pub[i].publish(mv);
             }
         }
         else{
-            float center_y=1/f_val0;
-            float back_center_x = ws_pos[2][0];
-            float hypotense = center_y * center_y + back_center_x * back_center_x;
-            float real_length = sqrt(hypotense);
-
             for(int i=0;i<4;i++){
               std_msgs::Float32 sv;
               std_msgs::Float32 mv;
               if (i == 0 or i == 2){
                 sv.data=-atan2(ws_pos[i][0],ws_pos[i][1]- center_y);
-                // mv.data=f_val1*(center_y-ws_pos[i][1])/center_y;
-
-                float side_length = sqrt((center_y - ws_pos[i][1])*(center_y - ws_pos[i][1]) + 
-                ws_pos[i][0] * ws_pos[i][0]);
-                mv.data=f_val1*(side_length - real_length)/real_length*1/2;
-
-                // float side_length = sqrt((center_y - ws_pos[i][1])*(center_y - ws_pos[i][1]) 
-                // - ws_pos[i][0] * ws_pos[i][0]);
-                // mv.data=f_val1*(side_length/center_y);
+                mv.data=f_val1*(hypotense-ws_pos[i][1])/hypotense*1/2;
               }
               else if(i == 1 or i == 3){
                 sv.data=-atan2(ws_pos[i][0],ws_pos[i][1]-center_y);
-                // mv.data=-f_val1*(ws_pos[i][1]-center_y)/center_y;
-
-                float side_length = sqrt((center_y - ws_pos[i][1])*(center_y - ws_pos[i][1]) + 
-                ws_pos[i][0] * ws_pos[i][0]);
-                mv.data=-f_val1*(side_length - real_length)/real_length*1/2;
-
-                // float side_length = sqrt((center_y + ws_pos[i][1])*(center_y + ws_pos[i][1]) 
-                // - ws_pos[i][0] * ws_pos[i][0]);
-                // mv.data=f_val1*(side_length/center_y);
+                mv.data=-f_val1*(ws_pos[i][1]-hypotense)/hypotense*1/2;
               }
               servo_pub[i].publish(sv);
               motor_pub[i].publish(mv);
